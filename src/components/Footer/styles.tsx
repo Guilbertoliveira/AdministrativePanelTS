647cea5c0a9a2ba508ceda0806ae09739f4896ab
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-07-31 11:10:28 -0300

    chore: improvements code

---
+++ src/components/Footer/styles.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const FooterStyled = styled.footer`
+height: 100%;
+color: white;
+padding: 1em;
+background-color: var(--dark-blue);
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+export const ListStyled = styled.ul`
+display: flex;
+justify-content: space-evenly;
+padding: 0px;
+`
+
+export const ItemStyled = styled.li`
+list-style: none;
+`
+
+export const DivStyled = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+`
