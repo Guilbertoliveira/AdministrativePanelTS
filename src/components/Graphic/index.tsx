89d5f1b56d43fb213dd8176202bddd114b0790ef
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-08-06 14:43:22 -0300

    chore: improvements tooltip

--- src/components/Graphic/index.tsx
+++ src/components/Graphic/index.tsx
@@ -2,6 +2,7 @@ import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis, CartesianGrid, Toolti
 import IProfessional from "../../types/IProfessional";
 import IQuery from "../../types/IQuery";
 import useDataGraphic from "./useDataGraphic";
+import { SectionStyled } from './styles'
 
 interface Props {
     professionals: IProfessional[] | null,
@@ -10,23 +11,38 @@ interface Props {
 
 interface IData {
     name: string,
-    query: number
+    query: number,
 }
+
+
+
+const CustomTooltip = ({ active, payload, label }: any) => {
+    if (active && payload && payload.length) {
+        return (
+            <div className="custom-tooltip">
+                <p className="label">{`Profissional: ${label}`}</p>
+                <p className="desc">{`Consultas feitas: ${payload[0].value}`}</p>
+            </div>
+        );
+    }
+
+    return null;
+};
 
 export default function Graphic({ professionals, query }: Props) {
     let data: Array<IData> = useDataGraphic({ professionals, query });
     console.log(data)
     return (
-        <>
+        <SectionStyled>
             <ResponsiveContainer width="100%" height={350}>
-                <BarChart layout="vertical" data={data} margin={{ top: 25, right: 40, bottom: 20 }}>
+                <BarChart layout="vertical" data={data} margin={{ top: 25, right: 40, bottom: 10, left: 40 }}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis type="number" ></XAxis>
-                    <Tooltip />
+                    <Tooltip content={<CustomTooltip />} />
                     <YAxis type="category" dataKey="name"></YAxis>
                     <Bar dataKey='query' fill="#083860" barSize={30}></Bar>
                 </BarChart>
             </ResponsiveContainer>
-        </>
+        </SectionStyled>
     )
 }
---
+++ src/components/Graphic/styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const SectionStyled = styled.section`
+background-color: var(--white);
+border-radius: 16px;
+margin-top: 10px;
+`
